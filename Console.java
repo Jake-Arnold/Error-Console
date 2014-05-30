f7e3adb3ae68de8e678e9a17dc94a0fca50090fe
Jake Arnold <rakeyjake@hotmail.co.uk>
2014-05-30 15:38:13 +0100

    Create Console.java

---
+++ Console.java
@@ -0,0 +1,85 @@
+import java.awt.Color;
+import java.awt.ScrollPane;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.WindowConstants;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
+public class Console {
+
+	private JFrame frame;
+	private final JTextPane outputText = new JTextPane();
+	private StyledDocument doc = outputText.getStyledDocument();
+	private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+	public Console() {
+		initialize();
+	}
+
+	public void initialize() {
+
+		outputText.setEditable(false);
+		
+		frame = new JFrame("Output") {
+			{
+				getContentPane().setLayout(
+						new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+				add(new ScrollPane() {
+					{
+						add(outputText);
+					}
+				});
+				
+				
+			}
+		};
+
+		frame.setBounds(200, 200, 600, 200);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	/**
+	 * Logs the given message in the console window.
+	 * 
+	 * @param message
+	 */
+	public void log(String message) {
+
+
+        Date currentTime = new Date(System.currentTimeMillis());
+        
+		try {
+			doc.insertString(doc.getLength(), "[" + sdf.format(currentTime) + "]: "+ message + "\n", null);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+	/**
+	 * Logs the given message in the console window in bold red.
+	 * 
+	 * @param message
+	 */
+	public void logError(String message) {
+		SimpleAttributeSet keyWord = new SimpleAttributeSet();
+		StyleConstants.setForeground(keyWord, Color.RED);
+		StyleConstants.setBold(keyWord, true);
+
+		 Date currentTime = new Date(System.currentTimeMillis());
+		 
+		try {
+			doc.insertString(doc.getLength(), "[" + sdf.format(currentTime) + "]: "+ message + "\n", keyWord);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+}
