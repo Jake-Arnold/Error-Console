c3ea8e14e047ad490ba4dc0bf75444c90edc5906
Jake Arnold <rakeyjake@hotmail.co.uk>
2014-05-31 13:02:07 +0100

    Updates like mad. A lot more documentation too.

--- src/Console.java
+++ src/Console.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
@@ -8,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFileChooser;
@@ -16,39 +16,55 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.WindowConstants;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
-import javax.swing.text.StyleContext;
 import javax.swing.text.StyledDocument;
 
 /**
+ * This class creates a JFrame in Java Swing with the capabilities to log
+ * messages and errors in it when instantiated.
  * 
  * @author RakeyJakey
  * 
  */
 public class Console {
 
+	/*
+	 * Swing Components
+	 */
 	private JFrame frame;
 	private final JTextPane outputText = new JTextPane();
 	private final JMenuItem save = new JMenuItem("Save as text file");
 	private final JMenuItem clear = new JMenuItem("Clear console");
-	private final JMenuItem close = new JMenuItem("Close");
-	private StyledDocument doc = outputText.getStyledDocument();
-	private SimpleAttributeSet keyWord = new SimpleAttributeSet();
-	private SimpleDateFormat simpleDateFormatter = new SimpleDateFormat("HH:mm");
+	private final JMenuItem terminate = new JMenuItem(
+			"Terminate Main Application");
+	private final JMenuItem close = new JMenuItem("Close console");
+	private JScrollBar scrollBar;
+
+	private final StyledDocument doc = outputText.getStyledDocument();
+	private final SimpleAttributeSet keyWord = new SimpleAttributeSet();
+	private final SimpleDateFormat simpleDateFormatter = new SimpleDateFormat(
+			"HH:mm");
 	private Date currentTime;
 
 	public Console() {
 		initialize();
 	}
 
-	@SuppressWarnings("serial")
-	public void initialize() {
+	/**
+	 * Initializes the JFrame and its content. Makes use of double {{
+	 * initialization.
+	 */
+	private void initialize() {
 
+		/*
+		 * Creates a JMenuBar and adds its content to it.
+		 */
 		final JMenuBar menuBar = new JMenuBar() {
 			{
 				add(new JMenu("File") {
@@ -57,20 +73,30 @@ public class Console {
 						add(clear);
 						addSeparator();
 						add(close);
+						add(terminate);
 					}
 				});
 			}
 		};
 
+		/*
+		 * Creates and populates the frame with a JMenu Bar and JScrollPane.
+		 */
 		frame = new JFrame("Console") {
 			{
 				getContentPane().setLayout(
 						new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
 				setJMenuBar(menuBar);
 
-				add(new ScrollPane() {
+				/*
+				 * Adds the JScrollPane and populates it with the JTextPane and
+				 * also initializes the JScrollBar variable.
+				 */
+
+				add(new JScrollPane() {
 					{
-						add(outputText);
+						scrollBar = getVerticalScrollBar();
+						getViewport().add(outputText);
 						outputText.setEditable(false);
 					}
 				});
@@ -78,7 +104,9 @@ public class Console {
 			}
 		};
 
-		// ActionListener added to save file.
+		/*
+		 * ActionListener added to save file.
+		 */
 		save.addActionListener(new ActionListener() {
 
 			@Override
@@ -87,7 +115,10 @@ public class Console {
 			}
 		});
 
-		// Action listener to clear console
+		/*
+		 * Action listener to clear console
+		 */
+
 		clear.addActionListener(new ActionListener() {
 
 			@Override
@@ -103,7 +134,9 @@ public class Console {
 			}
 		});
 
-		// Action listener for optional file-close instead of clicking x.
+		/*
+		 * Action listener for optional file-close instead of clicking x.
+		 */
 		close.addActionListener(new ActionListener() {
 
 			@Override
@@ -112,13 +145,38 @@ public class Console {
 			}
 		});
 
-		// SET IT RELATIVE TO THE MAIN APPLICATION.
+		/*
+		 * Action listener for the option to terminate the main application
+		 * alonside the console.
+		 */
+		terminate.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				System.exit(0);
+			}
+		});
+
+		/*
+		 * This sets the bounds as pack is not ideal for this. It will be needed
+		 * to be set relative to the main application that is using this
+		 * console.
+		 */
 		frame.setBounds(200, 200, 600, 200);
 
+		/*
+		 * Sets as visible and sets the default close operation as dispose on
+		 * close so that it does not terminate the main application when the
+		 * console is closed.
+		 */
 		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		frame.setVisible(true);
 
+		/*
+		 * This logs that the console is started.
+		 */
 		log("Console started...\n");
+
 	}
 
 	/**
@@ -132,6 +190,7 @@ public class Console {
 			doc.insertString(doc.getLength(),
 					"  [" + simpleDateFormatter.format(currentTime) + "]: "
 							+ message + "\n", null);
+			scrollBar.setValue(scrollBar.getMaximum());
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -151,23 +210,17 @@ public class Console {
 			doc.insertString(doc.getLength(),
 					"  [" + simpleDateFormatter.format(currentTime)
 							+ "] [ERROR]: " + message + "\n", keyWord);
+			scrollBar.setValue(scrollBar.getMaximum());
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 
 	}
 
-	// Main for testing.
-	public static void main(String[] args) {
-		Console console = new Console();
-
-		for (int i = 0; i < 1000; i++) {
-			if (i % 2 == 0)
-				console.log("" + i);
-			else
-				console.logError("" + i);
-		}
-	}
+	/**
+	 * This saves the content of the console as a text file to the desired
+	 * location when "File - Save as text file" is chosen.
+	 */
 
 	private void save() {
 		String text = outputText.getText();
@@ -232,5 +285,32 @@ public class Console {
 
 		}
 
+	}
+
+	/*
+	 * Main for testing.
+	 */
+	public static void main(String[] args) {
+
+		final Console console = new Console();
+		final Random random = new Random();
+
+		Runnable worker = new Runnable() {
+
+			@Override
+			public void run() {
+				for (int i = 1; i < 500; i++) {
+					console.log("" + i);
+
+					try {
+						Thread.sleep(random.nextInt(200));
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+
+		};
+
+		worker.run();
 	}
 }
